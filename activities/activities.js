504c7b71604fa6cc3225a751a53f76cf5813e70e
AntonGorov195 <91376550+AntonGorov195@users.noreply.github.com>
2023-08-22 15:05:58 +0300

    Added user add to activities

--- activities/activities.js
+++ activities/activities.js
@@ -1,24 +1,24 @@
 const usersList = document.querySelector(".users-list");
-const bringItemsList = document.querySelector(".bring-items-list");
 const addUserBtn = document.querySelector(".add-user");
-const addI = document.querySelector(".add-user");
+const myself = document.querySelector(".you");
 
-for (let i = 0; i < usersList.childElementCount; i++) {
-	const user = usersList.children[i];
+{
+	const username = myself.querySelector(".username");
+	username.innerText = sessionStorage.getItem("username");
 
-	const removeUserBtn = user.querySelector(".remove-user");
-	removeUserBtn.addEventListener("click", removeUser);
-
-	const addItemBtn = user.querySelector(".add-item");
-	addItemBtn.addEventListener("click", addItem);
-
+	const addItemBtn = myself.querySelector(".add-item");
+	addItemBtn.addEventListener("click", addItemYourself);
 }
+
+addUserBtn.addEventListener("click", addUser);
 function removeItem(event) {
+	const bringItemsList = event.target.closest(".bring-items-list");
 	const item = event.target.closest(".bring-item");
 	item.remove();
-	// if (bringItemsList.childElementCount === 0) {
-	// 	bringItemsList.style.display = "none";
-	// }
+
+	if (bringItemsList.childElementCount === 0) {
+		bringItemsList.style.display = "none";
+	}
 }
 /*
     <li class="bring-item item-not-deliverd">
@@ -30,12 +30,25 @@ function removeItem(event) {
 		<button class="remove-item">X</button>
 	</li>
  */
-function addItem(event) {
-    if (bringItemsList.childElementCount===0) {
-        bringItemsList.style.display = "flex";
-    }
+function addItemYourself(event) {
+	addItem(event);
 
-	const name = usersList.querySelector(".add-item-name");
+	const deliveryStatus = document.createElement("input");
+	deliveryStatus.classList.add("change-deliver");
+	deliveryStatus.setAttribute("type", "checkbox");
+
+	const bringItemsList = event.target.closest(".user").querySelector(".bring-items-list");
+	bringItemsList.lastChild.querySelector(".delivery-status-container").prepend(deliveryStatus);
+}
+function addItem(event) {
+	const user = event.target.closest(".user");
+	const bringItemsList = user.querySelector(".bring-items-list");
+
+	if (bringItemsList.childElementCount === 0) {
+		bringItemsList.style.display = "flex";
+	}
+
+	const name = user.querySelector(".add-item-name");
 
 	const itemElement = document.createElement("li");
 	itemElement.classList.add("bring-item", "item-not-deliverd");
@@ -45,12 +58,8 @@ function addItem(event) {
 	itemElement.appendChild(itemName);
 
 	const deliveryStatusContainer = document.createElement("label");
+	deliveryStatusContainer.classList.add("delivery-status-container");
 	deliveryStatusContainer.addEventListener("click", onDeliveryStatusChanged);
-
-	const deliveryStatus = document.createElement("input");
-	deliveryStatus.classList.add("change-deliver");
-	deliveryStatus.setAttribute("type", "checkbox");
-	deliveryStatusContainer.appendChild(deliveryStatus);
 
 	const deliveryStatusText = document.createElement("span");
 	deliveryStatusText.classList.add("deliver-text");
@@ -66,6 +75,55 @@ function addItem(event) {
 	itemElement.appendChild(removeItemBtn);
 
 	bringItemsList.appendChild(itemElement);
+}
+function addUser(event) {
+	// <il class="user">
+	// 	<div class="user-info">
+	// 		<label class="username">Name</label>
+	// 		<button class="remove-user">
+	// 			Remove
+	// 		</button>
+	// 	</div>
+	// 	<ul class="bring-items-list">
+	// 	</ul>
+	// 	<input type="text" class="add-item-name"><button class="add-item">Add item</button>
+	// </il>
+
+	const user = document.createElement("il");
+	user.classList.add("user");
+
+	const userInfo = document.createElement("div");
+	userInfo.classList.add("user-info");
+
+	const username = document.createElement("label");
+	username.classList.add("username");
+	username.innerText = "[INSERT USERNAME HERE]";
+	userInfo.appendChild(username);
+
+	const removeUserBtn = document.createElement("button");
+	removeUserBtn.classList.add("remove-user");
+	removeUserBtn.innerText = "Remove";
+	removeUserBtn.addEventListener("click",removeUser);
+	userInfo.appendChild(removeUserBtn);
+
+	user.appendChild(userInfo);
+
+	const bringItemsList = document.createElement("ul");
+	bringItemsList.classList.add("bring-items-list");
+	user.appendChild(bringItemsList);
+
+	const addItemName = document.createElement("input");
+	addItemName.classList.add("add-item-name");
+	addItemName.setAttribute("type", "text");
+	user.appendChild(addItemName);
+
+	const addItemBtn = document.createElement("button");
+	addItemBtn.classList.add("add-item");
+	addItemBtn.innerText = "Add item";
+	addItemBtn.addEventListener("click", addItem);
+	user.appendChild(addItemBtn);
+
+	usersList.appendChild(user);
 }
 function removeUser(event) {
 	const user = event.target.closest(".user");
