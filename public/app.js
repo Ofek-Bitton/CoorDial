e0aa14ea913905d020c7a16734ed16086e7bb77e
AntonGorov195 <91376550+AntonGorov195@users.noreply.github.com>
2024-01-04 20:40:17 +0200

    Rewrote large chunk of the code. Now it contains JSDOCs and html classes are prefixed "s_" if they are used for styling and "q_" for quering.

--- public/app.js
+++ public/app.js
@@ -1,23 +1,54 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
 // import {
 // 	getFirestore,
 // 	collection,
 // 	getDocs,
 // } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
 // import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-analytics.js";
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
+// const app = initializeApp(firebaseConfig);
 // const db = getFirestore();
 // const col = collection(db, "groups");
 // getDocs(col).then((snapshot)=> {console.log(snapshot.docs); })
 // const analytics = getAnalytics(app);
+
+/**
+ * @type {HTMLButtonElement}
+ */
+const loginBtn = document.getElementById("login");
+/**
+ * @type {HTMLInputElement}
+ */
+const nameField = document.getElementById("name-field");
+/**
+ * @type {HTMLInputElement}
+ */
+const passwordField = document.getElementById("password-field");
+/**
+ * @param {MouseEvent} event
+ */
+function login(event) {
+	if (nameField.value == "" || passwordField.value == "") {
+		return;
+	}
+	event.preventDefault();
+	localStorage.setItem(
+		"user-data",
+		JSON.stringify({ name: nameField.value, password: passwordField.value })
+	);
+	document.location.href = "create-group/create-group.html";
+}
+
+const lastLogin = JSON.parse(localStorage.getItem("user-data"));
+
+if (lastLogin != null) {
+	nameField.value = lastLogin.name;
+	passwordField.value = lastLogin.password;
+}
+
+loginBtn.addEventListener("click", login);
--- public/create-group/create-group.js
+++ public/create-group/create-group.js
@@ -10,142 +10,144 @@ import {
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
 import { firebaseConfig } from "../firebaseConfig.js";
 
+/**
+ * @typedef {HTMLElement & {groupName: String,groupDate:Date, groupId:any}} Group
+ */
 const app = initializeApp(firebaseConfig);
 const db = getFirestore();
 const dbGroupColRef = collection(db, "groups");
 
-const groupList = document.querySelector(".group-list");
-const groupAddBtn = document.querySelector(".add-group");
-const username = document.querySelector(".username");
-const groupTimeInput = document.querySelector(".create-group-time input");
-const groupNameInput = document.querySelector(".create-group-name input");
-
-groupAddBtn.addEventListener("click", addGroup);
-username.innerText = "Welcome, " + sessionStorage.getItem("username") + "!";
-
-// Load saved groups on page load
-document.addEventListener("DOMContentLoaded", loadSavedGroups);
-
-function getSavedGroups() {
-	const savedGroupsString = localStorage.getItem("saved-groups");
-	if (savedGroupsString) {
-		// Convert the JSON string back to an array of objects
-		return JSON.parse(savedGroupsString);
-	} else {
-		return [];
-	}
+let userData = JSON.parse(localStorage.getItem("user-data"));
+if (userData == null) {
+	console.error("No user data found.");
+	userData = { name: "NO_USER", password: "NO_USER" };
+} else if (userData.name === undefined) {
+	console.error("No name in user data found.");
+	userData["name"] = "NO_NAME";
+} else if (userData.password === undefined) {
+	console.error("No password in user data found.");
+	userData["password"] = "NO_PASSWORD";
 }
 
-function loadSavedGroups() {
-	// const savedGroups = getSavedGroups();
+/**
+ * @type {HTMLHeadingElement}
+ */
+const welcomeHeader = document.getElementById("welcome");
+welcomeHeader.innerText = "Welcome, " + userData.name;
 
-	getDocs(dbGroupColRef).then((snapshot) => {
-		snapshot.docs.forEach((doc) => {
-			const data = doc.data();
-			const group = createGroupElement(
-				data.name,
-				data.date.toDate()
-			);
-			group["dataId"] = doc.id;
-		});
+const groupsList = document.getElementById("groups");
+/**
+ * @type {HTMLInputElement}
+ */
+const newGroupName = document.getElementById("new-group-name");
+/**
+ * @type {HTMLInputElement}
+ */
+const newGroupDate = document.getElementById("new-group-date");
+/**
+ * @type {HTMLButtonElement}
+ */
+const createGroupBtn = document.getElementById("create-group-btn");
+createGroupBtn.addEventListener("click", createGroup);
+
+getDocs(dbGroupColRef).then((snapshot) => {
+	snapshot.docs.forEach((doc) => {
+		const data = doc.data();
+		const group = createGroupElement(data.name, data.date.toDate());
+		group["groupId"] = doc.id;
+		groupsList.appendChild(group);
 	});
-}
+});
 
-function addGroup(event) {
-	// const date = getDateString();
-	// if (date === null) {
-	// 	return;
-	// }
-	// console.log(date);
-
-	// Create and append the group element
-
-	const time = groupTimeInput.value;
-	const date = new Date(time);
-
-	const group = createGroupElement(groupNameInput.value, date);
-	addDoc(dbGroupColRef, { name: groupNameInput.value, date: date }).then(
-		(docRef) => {
-			group["dataId"] = docRef.id;
-		}
-	);
-	// Save the groups to localStorage
-	saveGroups();
-}
-
-// Array to store the opened group windows
-const openedGroupWindows = [];
-
+/**
+ * @param {string} name
+ * @param {Date} date
+ * @returns {HTMLElement}
+ */
 function createGroupElement(name, date) {
-	const group = document.createElement("div");
-	group.classList.add("group");
-	groupList.appendChild(group);
+	const group = document.createElement("il");
+	group.classList.add("s_group");
+	group.classList.add("q_group");
+	{
+		const nameElement = document.createElement("h2");
+		nameElement.classList.add("q_name");
+		nameElement.innerText = name;
+		group.appendChild(nameElement);
 
-	const groupName = document.createElement("div");
-	groupName.classList.add("group-name");
-	groupName.innerText = name;
-	group.appendChild(groupName);
+		const dateElement = document.createElement("h3");
+		dateElement.classList.add("q_date");
+		dateElement.innerText = date.toLocaleString("en-GB", {
+			hour: "2-digit",
+			minute: "2-digit",
+			day: "numeric",
+			year: "2-digit",
+			month: "numeric",
+		});
+		group.appendChild(dateElement);
 
-	const groupDate = document.createElement("div");
-	groupDate.classList.add("group-date");
-	groupDate["dataDate"] = date;
-	groupDate.innerText = toDateString(date);
-	group.appendChild(groupDate);
+		const enterBtn = document.createElement("button");
+		enterBtn.innerText = "Enter";
+		enterBtn.addEventListener("click", enterGroup);
+		group.appendChild(enterBtn);
 
-	const removeBtn = document.createElement("button");
-	removeBtn.innerText = "Remove";
-	group.appendChild(removeBtn);
+		const deleteBtn = document.createElement("button");
+		deleteBtn.classList.add("s_delete-btn");
+		deleteBtn.innerText = "Delete";
+		deleteBtn.addEventListener("click", deleteGroup);
+		group.appendChild(deleteBtn);
+	}
 
-	// Function to open the group entry in a new window
-	const openGroupEntry = () => {
-		// const groupInfo = `Group Name: ${name}<br>Group Date: ${date}`;
-		const groupInfo = { name: name, date: date };
-		window.location.replace("../group-view/group-view.html");
-		sessionStorage.setItem("groupInfo", JSON.stringify(groupInfo));
-	};
-
-	// Add the event listener to the "Remove" button
-	removeBtn.addEventListener("click", (e) => {
-		const groupToRemove = e.target.parentElement;
-
-		const name = groupToRemove.querySelector(".group-name").innerText;
-		const time = groupToRemove.querySelector(".group-date").dataDate;
-		const docRef = doc(db, "groups", groupToRemove.dataId);
-		deleteDoc(docRef)
-
-		groupToRemove.remove();
-		// saveGroups();
-	});
-
-	// Create the "View Group" button
-	const viewGroupBtn = document.createElement("button");
-	viewGroupBtn.innerText = "View Group";
-	group.appendChild(viewGroupBtn);
-
-	// Add the event listener to the "View Group" button
-	viewGroupBtn.addEventListener("click", openGroupEntry);
 	return group;
 }
-
-function saveGroups() {
-	const groupSave = [];
-	for (let i = 0; i < groupList.children.length; i++) {
-		// const name = groupList.children[i].querySelector(".group-name").innerText;
-		// const time = groupList.children[i].querySelector(".group-date").dataDate;
-	}
-	// localStorage.setItem("saved-groups", JSON.stringify(groupSave));
+/**
+ * @param {Event} event
+ */
+function enterGroup(event) {
+	/**
+	 * @type {Group}
+	 */
+	const group = event.target.closest(".q_group");
+	localStorage.setItem(
+		"group-data",
+		JSON.stringify({
+			name: group.groupName,
+		})
+	);
+	document.location.href = "../group-view/group-view.html";
 }
-function toDateString(date) {
-	return date.toLocaleString("en-GB", {
-		hour: "2-digit",
-		minute: "2-digit",
-		day: "numeric",
-		year: "2-digit",
-		month: "numeric",
-	});
-}
+/**
+ * @param {Event} event
+ */
+function createGroup(event) {
+	const name = newGroupName.value;
+	const date = new Date(newGroupDate.value);
 
-function closeGroupWindows() {
-	openedGroupWindows.forEach((window) => window.close());
-	openedGroupWindows.length = 0;
+	/**
+	 * @type {Group}
+	 */
+	const group = createGroupElement(name, date);
+	group["groupName"] = name;
+	group["groupDate"] = date;
+
+	addDoc(dbGroupColRef, { name: group.groupName, date: group.groupDate }).then(
+		(docRef) => {
+			group["groupId"] = docRef.id;
+		}
+	);
+
+	groupsList.appendChild(group);
+}
+/**
+ * @param {Event} event
+ */
+function deleteGroup(event) {
+	/**
+	 * @type {Group}
+	 */
+	const group = event.target.closest(".q_group");
+
+	const docRef = doc(db, "groups", group.groupId);
+	deleteDoc(docRef);
+
+	group.remove();
 }
